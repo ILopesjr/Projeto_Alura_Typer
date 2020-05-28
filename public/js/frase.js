98f3c11fc6a762227a509fd157b0de7ea3282292
Ivanildo Lopes da Silva Junior <ivanildo.lopes.jr@gmail.com>
2020-05-27 21:12:39 -0300

    adicionando função busca frase

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
     $(".spinner").show();
@@ -11,7 +12,7 @@ function fraseAleatoria(){
         }, 2500);
     }).always(function(){
         $(".spinner").toggle();
-    })
+    });
 }
 
 function trocaFraseAleatoria(data){
@@ -20,4 +21,28 @@ function trocaFraseAleatoria(data){
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhofrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase(){
+    $(".spinner").show();
+
+    let fraseId = $(".frase-id").val()-1;
+    let dados = { id: fraseId}
+
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $(".erro").toggle();
+        setTimeout(function(){
+            $(".erro").toggle();
+        }, 2500);
+    }).always(function(){
+        $(".spinner").toggle();
+    });
+}
+
+function trocaFrase(data){
+    let frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhofrase();
+    atualizaTempoInicial(data.tempo);
 }
